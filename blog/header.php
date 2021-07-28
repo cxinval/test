24f2decf8ee93e89754278531afa029ff05659dc
cxinval <cxinval7@mail.ru>
2021-07-28 17:42:30 +0300

    Обновился

---
+++ blog/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="css/main.css" />
+    <title>Test</title>
+  </head>
+  <body>
+    <header>
+      <div
+        class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom ">
+        <div class="container">
+          <div class="header-wrap">
+            <a
+              href="/"
+              class="d-flex align-items-center text-dark text-decoration-none"
+            >
+              <span class="fs-4">Книга</span>
+            </a>
+            <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+              <a class="me-3 py-2 text-dark text-decoration-none" href="#"
+                >Каталог</a
+              >
+              <a class="me-3 py-2 text-dark text-decoration-none" href="#"
+                >Автор</a
+              >
+              <a class="me-3 py-2 text-dark text-decoration-none" href="#"
+                >Контакты</a
+              >
+              <a class="py-2 text-dark text-decoration-none" href="blog/login.php">Войти</a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php require('blog/header.php');?>
+    <main>
+      <div class="container">
+        <div class="main-wrap">
+          <div class="col-3 text-center">
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm book-block">
+                <div class="card-header py-3">
+                  <h4 class="my-0 fw-normal book__title">PHP 7</h4>
+                  <img class="book-img" src="img/1.jpg" alt="" />
+                  <span class="book__year">Год издания: 31 мая 2017</span>
+                </div>
+                <div class="card-body">
+                  <button type="button" class="w-100 btn btn-lg btn-outline-primary"> Подробней </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-3 text-center">
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm book-block">
+                <div class="card-header py-3">
+                  <h4 class="my-0 fw-normal book__title">
+                    Компьютер глазами хакера
+                  </h4>
+                  <img class="book-img" src="img/2.jpg" alt="" />
+                  <span class="book__year">Год издания: 17 декабря 2012</span>
+                </div>
+                <div class="card-body">
+                  <button
+                    type="button"
+                    class="w-100 btn btn-lg btn-outline-primary"
+                  >
+                    Подробней
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-3 text-center">
+            <div class="col">
+              <div class="card mb-4 rounded-3 shadow-sm book-block">
+                <div class="card-header py-3">
+                  <h4 class="my-0 fw-normal book__title">
+                    PHP и MySQL. Разработка веб-приложений
+                  </h4>
+                  <img class="book-img" src="img/3.jpg" alt="" />
+                  <span class="book__year">Год издания: 29 июня 2016</span>
+                </div>
+                <div class="card-body">
+                  <button
+                    type="button"
+                    class="w-100 btn btn-lg btn-outline-primary"
+                  >
+                    Подробней
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+<?php require('blog/footer.php');?>   
+
